8ad4135395b1c2aceeb20700d2a2e2783c796186
Witoslaw Koczewski <wi@koczewski.de>
2019-02-21 17:29:29 +0100

    subscription as vector

--- src/material_desktop/api.cljs
+++ src/material_desktop/api.cljs
@@ -1,26 +1,22 @@
 (ns material-desktop.api
   (:require
-   [re-frame.core :as rf]))
+   [clojure.spec.alpha :as s]
+   [re-frame.core :as rf]
+   [conform.api :refer [validate]]))
 
+(s/def ::subscription-id    qualified-keyword?)
+(s/def ::subscription-args  map?)
+(s/def ::subscription       (s/cat :id   ::subscription-id
+                                   :args (s/? ::subscription-args)))
 
 (defn subscribe
-  ([subscription-name]
-   (subscribe subscription-name {}))
-  ([subscription-name subscription-args]
-   (if-not (qualified-keyword? subscription-name)
-     (throw (ex-info "Subscription name needs to be a qualified keyword."
-                     {:subscription-name subscription-name
-                      :subscription-args subscription-args})))
-   (if-not (or (nil? subscription-args) (map? subscription-args))
-     (throw (ex-info "Subscription args need to be a map."
-                     {:subscription-name subscription-name
-                      :subscription-args subscription-args})))
-   (rf/subscribe [subscription-name subscription-args])))
+  [subscription]
+  (validate ::subscribe
+            [:val subscription ::subscription])
+  (rf/subscribe subscription))
 
 
 (defn <subscribe
-  ([subscription-name]
-   (<subscribe subscription-name {}))
-  ([subscription-name subscription-args]
-   (if-let [signal (subscribe subscription-name subscription-args)]
-     @signal)))
+  [subscription]
+  (if-let [signal (subscribe subscription)]
+    @signal))

eb58e0ec084fc519fb9152478e7ec834d9b19ee7
Witoslaw Koczewski <wi@koczewski.de>
2019-02-22 13:55:12 +0100

    ddapi event integration with re-frame

--- src/material_desktop/ddapi_integration.cljs
+++ src/material_desktop/ddapi_integration.cljs
@@ -15,10 +15,10 @@
   (rf/reg-sub
    db-subscription-id
    (fn [db [_ {:as subscription-args :keys []}]]
-     (let [ db-instance-identifier (if db-instance-identifier-args-key
-                                     (get subscription-args
-                                          db-instance-identifier-args-key)
-                                     :singleton)]
+     (let [db-instance-identifier (if db-instance-identifier-args-key
+                                    (get subscription-args
+                                         db-instance-identifier-args-key)
+                                    :singleton)]
        (get-in db (api-db-path api-id db-instance-identifier))))))
 
 
@@ -36,6 +36,25 @@
            (ddapi/<query api-db query-id args)))))))
 
 
+(defn- reg-event
+  [event api-id db-instance-identifier-args-key]
+  (let [event-id (:id event)]
+    (rf/reg-event-db
+     event-id
+     (fn [db [_ args]]
+       (let [db-instance-identifier (if db-instance-identifier-args-key
+                                      (get args
+                                           db-instance-identifier-args-key)
+                                      :singleton)
+             path (api-db-path api-id db-instance-identifier)
+             args (dissoc args db-instance-identifier-args-key)
+             api-db (get-in db path)]
+         ;; TODO autocreate
+         (when-not api-db
+           (throw (ex-info (str "No api-db in app-db at path " path ".") {})))
+         (assoc-in db path (ddapi/events> api-db [[event-id args]])))))))
+
+
 
 (defn integrate-ddapis-into-re-frame
   []
@@ -43,14 +62,17 @@
     (let [api-id (:id api)
           db-instance-identifier-args-key (:db-instance-identifier-args-key api)
           db-subscription-id (keyword (name api-id) "db")
-          queries (vals (:queries api))]
+          queries (vals (:queries api))
+          events (vals (:events api))]
       (when (:autocreate-singleton-db? api)
         (rf/dispatch [:ddapi/create-db-if-missing {:api-id api-id
                                                    :db-instance-identifier :singleton
                                                    :db-args {}}]))
       (reg-db-sub api-id db-instance-identifier-args-key db-subscription-id)
       (doseq [query queries]
-        (reg-query-sub query api-id db-subscription-id db-instance-identifier-args-key)))))
+        (reg-query-sub query api-id db-subscription-id db-instance-identifier-args-key))
+      (doseq [event events]
+        (reg-event event api-id db-instance-identifier-args-key)))))
 
 
 (rf/reg-event-db
